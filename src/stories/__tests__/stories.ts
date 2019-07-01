8301f63fd771397cfdb78f08b9a4365fd19b2878
k-yokoishi <ss.xs.m713@gmail.com>
2019-07-01 20:33:11 +0900

    Add type of libs to supress warning

--- src/stories/__tests__/stories.ts
+++ src/stories/__tests__/stories.ts
@@ -1,7 +1,7 @@
 import initStoryshots from '@storybook/addon-storyshots';
 // `require.context()` in .storybook/config.ts is originally Webpack's func.
 // It enables `require.context()` for babel.
-import registerRequireContextHook from 'babel-plugin-require-context-hook/register';
+const registerRequireContextHook = require('babel-plugin-require-context-hook/register');
 registerRequireContextHook();
 
-initStoryshots();
+initStoryshots({});
