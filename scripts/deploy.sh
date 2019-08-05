0144ab23e0f60f883ccf86010b13df60b83028aa
k-yokoishi <ss.xs.m713@gmail.com>
2019-08-05 22:14:44 +0900

    Fix deploy process not to run CI task on gh-pages

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash -ex
 
 BUILD_DIR=build
+GIT_USERNAME=${GIT_USERNAME:-$(git config user.name)}
+GIT_EMAIL=${GIT_EMAIL:-$(git config user.email)}
 
 rimraf $BUILD_DIR
 craco $BUILD_DIR
 
 cpx ".circleci*/config.yml" $BUILD_DIR --verbose
 
-gh-pages -d $BUILD_DIR
+gh-pages -d $BUILD_DIR --dotfiles -u "$GIT_USERNAME <$GIT_EMAIL>"
