44e970051b61efebd8d6f8f80d40782e01a91f7c
k-yokoishi <ss.xs.m713@gmail.com>
2019-08-12 11:53:32 +0900

    Adjust margin

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import 'typeface-roboto';
 import { Provider } from 'react-redux';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import store from './redux/store';
 import Header from './components/Header';
 import Prompt from './container/Prompt';
@@ -11,26 +12,46 @@ import PromptPreview from './container/PromptPreview';
 import ZshPreview from './container/ZshPreview';
 import PromptEdition from './container/PromptItemEdition';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    gridItem: {
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+    },
+  })
+);
+
 function App() {
+  const classes = useStyles();
   return (
     <div>
       <Provider store={store}>
         <Header />
-        <Grid container direction="row" justify="space-between" spacing={2}>
+        <Grid container direction="row" justify="space-between" spacing={3}>
           <Grid item xs={2}>
             <PromptItemList />
           </Grid>
           <Grid item xs={8}>
-            <Typography variant="h5">PROMPT</Typography>
-            <Prompt type="prompt" />
-            <Typography variant="h5">RPROMPT</Typography>
-            <Prompt type="rprompt" />
-            <Typography variant="h5">Preview</Typography>
-            <PromptPreview />
-            <Typography variant="h5">Paste your .zshrc</Typography>
-            <ZshPreview />
+            <Grid container direction="column">
+              <Grid item className={classes.gridItem}>
+                <Typography variant="h5">PROMPT</Typography>
+                <Prompt type="prompt" />
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <Typography variant="h5">RPROMPT</Typography>
+                <Prompt type="rprompt" />
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <Typography variant="h5">Preview</Typography>
+                <PromptPreview />
+              </Grid>
+              <Grid item className={classes.gridItem}>
+                <Typography variant="h5">Paste your .zshrc</Typography>
+                <ZshPreview />
+              </Grid>
+            </Grid>
           </Grid>
-          <Grid item xs={2}>
+          <Grid item xs={2} className={classes.gridItem}>
             <PromptEdition />
           </Grid>
         </Grid>
