31577086934f860acc8e7685b8b530b46ab106fd
k-yokoishi <ss.xs.m713@gmail.com>
2019-08-15 10:24:05 +0900

    Add copy button to copy PROMPT env var for zshrc

--- src/components/ZshrcPreview.tsx
+++ src/components/ZshrcPreview.tsx
@@ -1,5 +1,9 @@
 import * as React from 'react';
+import copy from 'clipboard-copy';
+import FileCopyOutlinedIcon from '@material-ui/icons/FileCopyOutlined';
+import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
+import Tooltip from '@material-ui/core/Tooltip';
 import { makeStyles } from '@material-ui/styles';
 import { IPromptItem } from '../types';
 
@@ -17,12 +21,22 @@ const useStyles = makeStyles({
     padding: '8px',
     color: 'white',
     backgroundColor: '#3E3A39',
+    position: 'relative',
+  },
+  copyIconButton: {
+    position: 'absolute',
+    top: '-4px',
+    right: '0px',
+  },
+  copyIcon: {
+    color: 'white',
   },
 });
 
 export default function ZshrcPreview(props: Props) {
   const { promptItems, rpromptItems = [] } = props;
   const classes = useStyles();
+  const [copied, setCopied] = React.useState<boolean>(false);
 
   const toPreview = (part: PromptItem): string => {
     let { shStr, fgColor, bgColor } = part;
@@ -41,8 +55,26 @@ export default function ZshrcPreview(props: Props) {
     return shStr;
   };
 
+  const onCopy = () => {
+    copy(
+      `PROMPT="${promptItems.map(p => toPreview(p))}"` +
+        (rpromptItems.length > 0 ? `\nRPROMPT="${rpromptItems.map(p => toPreview(p))}"` : '')
+    );
+    setCopied(true);
+  };
+
   return (
     <Typography component="div" className={classes.zshrcPreview}>
+      <Tooltip
+        placement="top-start"
+        title={copied ? 'Copied!' : 'Copy to clipboard'}
+        className={classes.copyIconButton}
+        onOpen={() => setCopied(false)}
+      >
+        <IconButton aria-label="copy" onClick={onCopy}>
+          <FileCopyOutlinedIcon className={classes.copyIcon} />
+        </IconButton>
+      </Tooltip>
       <code>PROMPT="{promptItems.map(p => toPreview(p))}"</code>
       <br />
       {rpromptItems.length > 0 && <code>RPROMPT="{rpromptItems.map(p => toPreview(p))}"</code>}
