8baf5d5583fd47e65e6f0198832c5184b0957aec
k-yokoishi <ss.xs.m713@gmail.com>
2019-08-16 16:29:34 +0900

    Set favicon

--- src/components/LogoText.tsx
+++ src/components/LogoText.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Typography, { TypographyProps } from '@material-ui/core/Typography';
 
-const LogoText: React.FC<TypographyProps> = ({ children, ...restProps }) => (
+const LogoText: React.FC<TypographyProps> = ({ children, style = {}, ...restProps }) => (
   <Typography
     style={{
       fontFamily: "'Kaushan Script', cursive",
       fontWeight: 'bold',
       color: 'white',
+      ...style,
     }}
     {...restProps}
   >
